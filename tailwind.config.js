8c6208998f56d08d207d8e841699efc96cee2105
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-06-29 13:13:50 +0530

    color addaed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#0A66C2',
+        'custom-twitter': '#00ACEE',
+        'custom-instagram': '#E4405F',
+        'color-1':'#0C133B',
+        'color-2':'#32D3E7',
+        'color-3':'#EBF4F1',
+        'color-4':'#D1E0DB'
+      },
+    },
   },
   plugins: [],
 }
