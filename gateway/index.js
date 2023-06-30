dd0ac03c87c34aa1886543a5dcf7a2a78e1b4ba6
Anandhu <anandhuashok.k@gmail.com>
2023-06-30 11:26:14 +0530

    gateway updated

--- gateway/index.js
+++ gateway/index.js
@@ -14,6 +14,8 @@ app.use("/inventory", proxy("http://localhost:8003"));
 app.use("/orders", proxy("http://localhost:8004"));
 app.use("/search", proxy("http://localhost:8005"));
 app.use("/wishlist", proxy("http://localhost:8006"));
+app.use("/admin", proxy("http://localhost:8007"));
+app.use("/product", proxy("http://localhost:8008"));
 
 app.listen(PORT, () => {
   console.log("Gateway is Listening to Port "+ PORT);
