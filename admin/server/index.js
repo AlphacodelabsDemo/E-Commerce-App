b9288ebff1db0e3607b4b127b2f1deb3f8b8b2e0
sumesh <sumesh9050@gmail.com>
2023-06-30 15:01:29 +0530

    signup,login and Jwt auth

--- admin/server/index.js
+++ admin/server/index.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import adminRoutes from './routes/adminRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 9002;
 const MONGO_URI = process.env.MONGO_URI;
